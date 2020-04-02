b86bd299c336b8a25e8fc26cb0f8ea623bcfceec
Eric Born <eric.born85@gmail.com>
2020-04-01 18:15:20 -0700

    Update wk3.sql

--- wk3.sql
+++ wk3.sql
@@ -7,12 +7,14 @@ ShipType VARCHAR(100),
 Nationality VARCHAR(100)
 );
 
+--DROP TABLE Fact_Trip
 CREATE TABLE Fact_Trip (
-LogbookIdent INT FOREIGN KEY REFERENCES Dim_Ship(Dim_Ship_Key),
-RecID INT
+Fact_Trip_Key INT IDENTITY(1,1) PRIMARY KEY,
+Dim_Ship_Key INT FOREIGN KEY REFERENCES Dim_Ship(Dim_Ship_Key),
+RecID INT,
+Date DATE,
 Rain BIT,
 Fog BIT,
 Snow BIT,
-Thunder BIT,
-Date DATE,
+Thunder BIT
 );
