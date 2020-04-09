59bdc975eed5ebcbbd70b7c62cf8b64b93a84f46
Eric Born <eric.born85@gmail.com>
2020-04-08 20:46:24 -0700

    Update DW - ETL.sql

--- DW - ETL.sql
+++ DW - ETL.sql
@@ -13,7 +13,7 @@ BEGIN
     SELECT 'Olist_DW database has been created'
 END;
 
-USE Opioids_DW
+USE Opioids_DW;
 
 -- Code to setup the distributor table within the warehouse
 --DROP SEQUENCE distributor_key
@@ -65,40 +65,91 @@ INTO relabeler
 FROM (SELECT DISTINCT combined_labeler_name
 	  FROM Opioids.dbo.arcos) ar;
 
+-- Created an index to help with reading data from the arcos table
+USE Opioids
+CREATE NONCLUSTERED INDEX indx_1 ON dbo.arcos
+([reporter_name], [reporter_address1]) include ([buyer_name], [buyer_address1], [drug_name],[quantity], [transaction_Date], [Combined_Labeler_Name]);
 
+-- function found on stack overflow for identifying non-numeric values when trying to convert between varchar and float
+-- https://stackoverflow.com/questions/8085015/error-converting-varchar-to-float
 
+CREATE FUNCTION dbo.isReallyNumeric  
+(  
+    @num VARCHAR(64)  
+)  
+RETURNS BIT  
+BEGIN  
+    IF LEFT(@num, 1) = '-'  
+        SET @num = SUBSTRING(@num, 2, LEN(@num))  
+
+    DECLARE @pos TINYINT  
+
+    SET @pos = 1 + LEN(@num) - CHARINDEX('.', REVERSE(@num))  
+
+    RETURN CASE  
+    WHEN PATINDEX('%[^0-9.-]%', @num) = 0  
+        AND @num NOT IN ('.', '-', '+', '^') 
+        AND LEN(@num)>0  
+        AND @num NOT LIKE '%-%' 
+        AND  
+        (  
+            ((@pos = LEN(@num)+1)  
+            OR @pos = CHARINDEX('.', @num))  
+        )  
+    THEN  
+        1  
+    ELSE  
+    0  
+    END  
+END  
+GO;
+
+-- Using the above function, I'm performing an update to set all records that have a non-numeric value
+-- in the dosage_unit column to 999
+UPDATE Opioids.dbo.arcos
+SET dosage_unit = 999
+WHERE dbo.isReallyNumeric(dosage_unit) = 0;
+
+-- Same for the quantity and CALC_BASE_WT_IN_GM columns
+UPDATE Opioids.dbo.arcos
+SET quantity = 999
+WHERE dbo.isReallyNumeric(quantity) = 0;
+
+UPDATE Opioids.dbo.arcos
+SET CALC_BASE_WT_IN_GM = 0
+WHERE dbo.isReallyNumeric(CALC_BASE_WT_IN_GM) = 0;
 
 --DROP TABLE orders
 -- Gathers the initial data from the Opioids database and insert it into a table called transactions in the Opioids_DW database
 -- does a convert on the time_period.date_key from INT to DATE
--- also converts orders order_purchase_timestamp from DATETIME to DATE
--- filters out any canceled orders and only orders earlier than 2019 for SSIS demonstration purposes
-USE Opioids_DW
-SELECT t.date_key, di.distributor_key, b.buyer_key, dr.drug_key, r.relabeler_key,
-AVG(ar.quantity) AS 'Avg_Qty', SUM(ar.quantity) AS 'Sum_QTY',
-AVG(ar.dosage_unit) AS 'Avg_Dose', SUM(ar.dosage_unit) AS 'Sum_Dose',
-AVG(ar.CALC_BASE_WT_IN_GM) AS 'Avg_Grams', SUM(ar.CALC_BASE_WT_IN_GM) AS 'Sum_Grams'
---INTO Opioids_DW.dbo.transactions
+-- Also converts using RIGHT AND LEFT to transform the arcos data format from MMDDYYYY to YYYYMMDD
+USE Opioids_DW;
+
+SELECT top 10 t.date_key, di.distributor_key, b.buyer_key, dr.drug_key, r.relabeler_key,
+ROUND(AVG(CAST(ar.quantity AS FLOAT)),3) AS 'Avg_Qty', SUM(CAST(ar.quantity AS FLOAT)) AS 'Sum_QTY',
+ROUND(AVG(CAST(ar.dosage_unit AS FLOAT)),3) AS 'Avg_Dose', SUM(CAST(ar.dosage_unit AS FLOAT)) AS 'Sum_Dose',
+ROUND(AVG(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'Avg_Grams', ROUND(SUM(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'Sum_Grams'
+INTO Opioids_DW.dbo.transactions
 FROM Opioids.dbo.arcos ar
-INNER JOIN Opioids_DW.dbo.time_period t ON CONVERT(DATE,CONVERT(VARCHAR(8),t.date_key,112)) = CONVERT(DATE,ar.transaction_date,112)
+INNER JOIN Opioids_DW.dbo.time_period t ON CONVERT(DATE,CONVERT(VARCHAR(8),t.date_key,112)) = CONVERT(DATE,(RIGHT(transaction_date,4)+LEFT(transaction_date,4)),112)
 INNER JOIN Opioids_DW.dbo.distributor di ON di.distributor_name = ar.REPORTER_NAME AND di.distributor_address = ar.REPORTER_ADDRESS1
 INNER JOIN Opioids_DW.dbo.buyer b ON b.buyer_name = ar.BUYER_NAME AND b.buyer_address = ar.BUYER_ADDRESS1
 INNER JOIN Opioids_DW.dbo.drug dr ON dr.drug_name = ar.DRUG_NAME
 INNER JOIN Opioids_DW.dbo.relabeler r ON r.relabeler_name = ar.Combined_Labeler_Name
-GROUP BY t.date_key, di.distributor_key, b.buyer_key, dr.drug_key, r.relabeler_key
+GROUP BY t.date_key, di.distributor_key, b.buyer_key, dr.drug_key, r.relabeler_key;
 
-SELECT * FROM time_period
 
-l.location_key, p2.product_key, oi.seller_id, 
-SUM(oi.price) AS 'sales_total', COUNT(oi.product_id) AS 'sales_quantity'
-INTO Olist_DW.dbo.orders
-FROM Olist_Orders.dbo.orders o
-INNER JOIN Olist_Orders.dbo.order_items oi ON oi.order_id = o.order_id
-INNER JOIN Olist_Orders.dbo.products p ON p.product_id = oi.product_id
-INNER JOIN Olist_Orders.dbo.category c ON c.product_category_name = p.product_category_name
-INNER JOIN Olist_DW.dbo.product p2 ON p2.product = c.Product_category_name_english
-INNER JOIN Olist_Orders.dbo.sellers s ON s.seller_id = oi.seller_id
-INNER JOIN Olist_DW.dbo.time_period t ON CONVERT(DATE,CONVERT(VARCHAR(8),t.date_key,112)) = CONVERT(DATE,o.order_purchase_timestamp,112)
-INNER JOIN Olist_DW.dbo.location l ON l.z-ip = s.seller_zip_code_prefix AND l.city = s.seller_city
-WHERE o.order_status != 'canceled' AND order_purchase_timestamp < '20190101'
-GROUP BY t.date_key, l.location_key, p2.product_key, oi.seller_id;
+SELECT TOP 10 *
+FROM buyer
+
+SELECT TOP 10 *
+FROM distributor
+
+SELECT TOP 10 *
+FROM drug
+
+SELECT TOP 10 *
+FROM relabeler
+
+SELECT TOP 10 *
+FROM time_period
