9d075284d12024f6de8551aa2c0b71955036ac90
Eric Born <eric.born85@gmail.com>
2020-04-09 17:03:50 -0700

    Update wk4.sql

--- wk4.sql
+++ wk4.sql
@@ -30,15 +30,10 @@ SELECT TOP 10 *
 FROM [dbo].[PERSON_CUSTOMER_DIM];
 
 -- Part 3.
-SELECT TOP 10 MANUFACTURE_YEAR, FACTORY_LABEL, PRODUCT_DESCRIPTION, QTY_PASSED, QTY_FAILED
-FROM MANUFACTURE_FACT mf
-JOIN CALENDAR_MANUFACTURE_DIM cm ON cm.MANUFACTURE_CAL_KEY = mf.MANUFACTURE_CAL_KEY
-JOIN PRODUCT_DIM pd ON pd.PRODUCT_KEY = mf.PRODUCT_KEY
-JOIN FACTORY_DIM fd ON fd.FACTORY_KEY = mf.FACTORY_KEY;
-
 -- Identify the three highest product unit output factories for each year
 -- uses a subquery with rank on the sum of the QTY_PASSED, partitioned by the production year
-SELECT * FROM (
+SELECT [Year], [Factory Name], [Product Description], [Total Units produced], [Total Units rejected]
+ FROM (
 SELECT RIGHT(MANUFACTURE_YEAR,4) AS 'Year', FACTORY_LABEL AS 'Factory Name', PRODUCT_DESCRIPTION AS 'Product Description', 
 SUM(QTY_PASSED) AS 'Total Units produced', SUM(QTY_FAILED) AS 'Total Units rejected', 
 RANK() OVER(PARTITION BY MANUFACTURE_YEAR ORDER BY SUM(QTY_PASSED) DESC) Qty_Rank
