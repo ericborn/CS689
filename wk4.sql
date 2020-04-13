8be51f64da31657d0d8004e8152363c71aca1044
Eric Born <eric.born85@gmail.com>
2020-04-13 11:39:55 -0700

    Update wk4.sql

--- wk4.sql
+++ wk4.sql
@@ -31,12 +31,12 @@ FROM [dbo].[PERSON_CUSTOMER_DIM];
 
 -- Part 3A.
 -- Identify the three highest product unit output factories for each year
--- uses a subquery with rank on the sum of the QTY_PASSED, partitioned by the production year
+-- Uses a subquery with rank on the sum of the QTY_PASSED added with QTY_FAILED, partitioned by the production year
 SELECT [Year], [Factory Name], [Product Description], [Total Units produced], [Total Units rejected]
  FROM (
 SELECT RIGHT(MANUFACTURE_YEAR,4) AS 'Year', FACTORY_LABEL AS 'Factory Name', PRODUCT_DESCRIPTION AS 'Product Description', 
 SUM(QTY_PASSED) AS 'Total Units produced', SUM(QTY_FAILED) AS 'Total Units rejected', 
-RANK() OVER(PARTITION BY MANUFACTURE_YEAR ORDER BY SUM(QTY_PASSED) DESC) Qty_Rank
+RANK() OVER(PARTITION BY MANUFACTURE_YEAR ORDER BY SUM(QTY_PASSED + QTY_FAILED) DESC) Qty_Rank
 FROM MANUFACTURE_FACT mf
 JOIN CALENDAR_MANUFACTURE_DIM cm ON cm.MANUFACTURE_CAL_KEY = mf.MANUFACTURE_CAL_KEY
 JOIN PRODUCT_DIM pd ON pd.PRODUCT_KEY = mf.PRODUCT_KEY
@@ -44,6 +44,14 @@ JOIN FACTORY_DIM fd ON fd.FACTORY_KEY = mf.FACTORY_KEY
 GROUP BY MANUFACTURE_YEAR, FACTORY_LABEL, PRODUCT_DESCRIPTION) t
 WHERE Qty_Rank <= 3
 ORDER BY Year
+
+select top 10 MANUFACTURE_YEAR, FACTORY_LABEL, PRODUCT_DESCRIPTION, SUM(QTY_PASSED + QTY_FAILED) AS 'Total'
+FROM MANUFACTURE_FACT mf
+JOIN CALENDAR_MANUFACTURE_DIM cm ON cm.MANUFACTURE_CAL_KEY = mf.MANUFACTURE_CAL_KEY
+JOIN PRODUCT_DIM pd ON pd.PRODUCT_KEY = mf.PRODUCT_KEY
+JOIN FACTORY_DIM fd ON fd.FACTORY_KEY = mf.FACTORY_KEY
+GROUP BY MANUFACTURE_YEAR, FACTORY_LABEL, PRODUCT_DESCRIPTION
+ORDER BY [Total] desc
 
 -- Part 3B.
 -- Subquery in the where statement uses rank to find the highest production year.
