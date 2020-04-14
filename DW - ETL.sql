9b52f4d7e5baedeff50632a9fe0de83e42cfcbff
Eric Born <eric.born85@gmail.com>
2020-04-14 12:49:52 -0700

    Update DW - ETL.sql

--- DW - ETL.sql
+++ DW - ETL.sql
@@ -15,8 +15,8 @@ END;
 
 USE Opioids_DW;
 
--- Code to setup the distributor table within the warehouse
---DROP SEQUENCE distributor_key
+-- Code to setup the distributor_dim table within the warehouse
+--DROP SEQUENCE distributor_dim_key
 CREATE SEQUENCE distributor_key
 START WITH 1000
 INCREMENT BY 1;
@@ -24,12 +24,12 @@ INCREMENT BY 1;
 SELECT NEXT VALUE FOR distributor_key AS distributor_key, 
 ar.REPORTER_NAME AS 'distributor_name', ar.REPORTER_ADDRESS1 AS 'distributor_address', 
 ar.REPORTER_CITY AS 'distributor_city', ar.REPORTER_STATE AS 'distributor_state', ar.REPORTER_ZIP AS 'distributor_zip', ar.REPORTER_COUNTY AS 'distributor_county'
-INTO distributor
+INTO distributor_dim
 FROM (SELECT DISTINCT REPORTER_NAME, REPORTER_ADDRESS1, REPORTER_CITY, REPORTER_STATE, REPORTER_ZIP, REPORTER_COUNTY
 	  FROM Opioids.dbo.arcos) ar;
-
--- Code to setup the buyer table within the warehouse
---DROP SEQUENCE buyer_key
+  
+-- Code to setup the buyer_dim table within the warehouse
+--DROP SEQUENCE buyer_dim_key
 CREATE SEQUENCE buyer_key
 START WITH 10
 INCREMENT BY 1;
@@ -37,31 +37,30 @@ INCREMENT BY 1;
 SELECT NEXT VALUE FOR buyer_key AS buyer_key, 
 ar.BUYER_NAME AS 'buyer_name', ar.BUYER_ADDRESS1 AS 'buyer_address', 
 ar.BUYER_CITY AS 'buyer_city', ar.BUYER_STATE AS 'buyer_state', ar.BUYER_ZIP AS 'buyer_zip', ar.BUYER_COUNTY AS 'buyer_county'
-INTO buyer
+INTO buyer_dim
 FROM (SELECT DISTINCT BUYER_NAME, BUYER_ADDRESS1, BUYER_CITY, BUYER_STATE, BUYER_ZIP, BUYER_COUNTY
 	  FROM Opioids.dbo.arcos) ar;
-
--- Code to setup the drug table within the warehouse
---DROP SEQUENCE drug_key
+	  
+-- Code to setup the drug_dim table within the warehouse
+--DROP SEQUENCE drug_dim_key
 CREATE SEQUENCE drug_key
 START WITH 1
 INCREMENT BY 1;
 
-SELECT NEXT VALUE FOR drug_key AS drug_key, 
-ar.DRUG_NAME AS 'drug_name'
-INTO drug
+SELECT NEXT VALUE FOR drug_key AS drug_key, ar.DRUG_NAME AS 'drug_name'
+INTO drug_dim
 FROM (SELECT DISTINCT DRUG_NAME
 	  FROM Opioids.dbo.arcos) ar;
 
--- Code to setup the relabeler table within the warehouse
---DROP SEQUENCE relabeler_key
+-- Code to setup the relabeler_dim table within the warehouse
+--DROP SEQUENCE relabeler_dim_key
 CREATE SEQUENCE relabeler_key
 START WITH 1
 INCREMENT BY 1;
 
 SELECT NEXT VALUE FOR relabeler_key AS relabeler_key, 
 ar.combined_labeler_name AS 'relabeler_name'
-INTO relabeler
+INTO relabeler_dim
 FROM (SELECT DISTINCT combined_labeler_name
 	  FROM Opioids.dbo.arcos) ar;
 
@@ -131,38 +130,38 @@ SET CALC_BASE_WT_IN_GM = 0
 WHERE dbo.isReallyNumeric(CALC_BASE_WT_IN_GM) = 0;
 
 --DROP TABLE orders
--- Gathers the initial data from the Opioids database and insert it into a table called transactions in the Opioids_DW database
+-- Gathers the initial data from the Opioids database and insert it into a table called transactions_fact in the Opioids_DW database
 -- does a convert on the time_period.date_key from INT to DATE
 -- Also converts using RIGHT AND LEFT to transform the arcos data format from MMDDYYYY to YYYYMMDD
 USE Opioids_DW;
 
-SELECT t.Month, t.Year, di.distributor_key, b.buyer_key, dr.drug_key, r.relabeler_key, COUNT(buyer_key) AS 'Total_Transactions',
+SELECT t.Month, t.Year, di.distributor_key, b.buyer_key, dr.drug_key, r.relabeler_key, COUNT(buyer_key) AS 'Total_transactions_fact',
 ROUND(AVG(CAST(ar.quantity AS FLOAT)),3) AS 'Average_Quantity', SUM(CAST(ar.quantity AS FLOAT)) AS 'Total_Quantity',
 ROUND(AVG(CAST(ar.dosage_unit AS FLOAT)),3) AS 'Average_Doses', SUM(CAST(ar.dosage_unit AS FLOAT)) AS 'Total_Doses',
 ROUND(AVG(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'Average_Grams', ROUND(SUM(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'Total_Grams'
-INTO Opioids_DW.dbo.transactions
+INTO Opioids_DW.dbo.transactions_fact
 FROM Opioids.dbo.arcos ar
 INNER JOIN Opioids_DW.dbo.time_period t ON CONVERT(DATE,CONVERT(VARCHAR(8),t.date_key,112)) = CONVERT(DATE,(RIGHT(transaction_date,4)+LEFT(transaction_date,4)),112)
-INNER JOIN Opioids_DW.dbo.distributor di ON di.distributor_name = ar.REPORTER_NAME AND di.distributor_address = ar.REPORTER_ADDRESS1
-INNER JOIN Opioids_DW.dbo.buyer b ON b.buyer_name = ar.BUYER_NAME AND b.buyer_address = ar.BUYER_ADDRESS1
-INNER JOIN Opioids_DW.dbo.drug dr ON dr.drug_name = ar.DRUG_NAME
-INNER JOIN Opioids_DW.dbo.relabeler r ON r.relabeler_name = ar.Combined_Labeler_Name
+INNER JOIN Opioids_DW.dbo.distributor_dim di ON di.distributor_name = ar.REPORTER_NAME AND di.distributor_address = ar.REPORTER_ADDRESS1
+INNER JOIN Opioids_DW.dbo.buyer_dim b ON b.buyer_name = ar.buyer_NAME AND b.buyer_address = ar.buyer_ADDRESS1
+INNER JOIN Opioids_DW.dbo.drug_dim dr ON dr.drug_name = ar.drug_NAME
+INNER JOIN Opioids_DW.dbo.relabeler_dim r ON r.relabeler_name = ar.Combined_Labeler_Name
 GROUP BY t.Month, t.Year, di.distributor_key, b.buyer_key, dr.drug_key, r.relabeler_key;
 
 SELECT TOP 10 *
-FROM buyer
+FROM buyer_dim
 
 SELECT TOP 10 *
-FROM distributor
+FROM distributor_dim
 
 SELECT TOP 10 *
-FROM drug
+FROM drug_dim
 
 SELECT TOP 10 *
-FROM relabeler
+FROM relabeler_dim
 
 SELECT TOP 10 *
 FROM time_period
 
 SELECT TOP 10 *
-FROM transactions
+FROM transactions_fact
