e8195c4c215d32fb2007374fdc456ce4b1ebc0be
Eric Born <eric.born85@gmail.com>
2020-04-23 19:07:25 -0700

    Update dw sum tests.sql

--- dw sum tests.sql
+++ dw sum tests.sql
@@ -18,7 +18,7 @@ AND dosage_unit != '999' AND quantity != '999' AND CALC_BASE_WT_IN_GM != '0'
 -- 1,421,580 -- 2006 jan
 
 -- 1,421,580
--- 1,534,180
+-- 1,421,580
 
 
 
@@ -26,10 +26,10 @@ AND dosage_unit != '999' AND quantity != '999' AND CALC_BASE_WT_IN_GM != '0'
 -- 52,870,220 AMERISOURCEBERGEN DRUG CORP
 SELECT DISTINCT t.date_key, di.distributor_key, 
 b.buyer_key, 
-dr.drug_key, --COUNT(buyer_key) AS 'total_transactions', 
---ROUND(AVG(CAST(ar.quantity AS FLOAT)),3) AS 'average_pill_quantity', SUM(CAST(ar.quantity AS FLOAT)) AS 'total_pill_quantity',
-ROUND(AVG(CAST(ar.dosage_unit AS FLOAT)),3) AS 'average_doses', SUM(CAST(ar.dosage_unit AS FLOAT)) AS 'total_doses'--,
---ROUND(AVG(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'average_grams', ROUND(SUM(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'total_grams'
+dr.drug_key, COUNT(buyer_key) AS 'total_transactions', 
+ROUND(AVG(CAST(ar.quantity AS FLOAT)),3) AS 'average_boxes', SUM(CAST(ar.quantity AS FLOAT)) AS 'total_boxes',
+ROUND(AVG(CAST(ar.dosage_unit AS FLOAT)),3) AS 'average_pills', SUM(CAST(ar.dosage_unit AS FLOAT)) AS 'total_pills',
+ROUND(AVG(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'average_grams', ROUND(SUM(CAST(ar.CALC_BASE_WT_IN_GM AS FLOAT)),3) AS 'total_grams'
 --INTO Opioids_DW.dbo.transactions_ma_fact
 FROM Opioids.dbo.arcos ar
 INNER JOIN Opioids_DW.dbo.time_period_dim t ON t.year = RIGHT(ar.transaction_Date,4) AND t.month = left(ar.transaction_Date,2) 
@@ -38,8 +38,7 @@ AND ar.REPORTER_CITY = di.distributor_city AND ar.REPORTER_ZIP = di.distributor_
 INNER JOIN Opioids_DW.dbo.buyer_dim b ON b.buyer_address = ar.buyer_ADDRESS1 AND b.buyer_city = ar.BUYER_CITY
 AND b.buyer_zip = ar.BUYER_ZIP AND b.buyer_dea_num = ar.BUYER_DEA_NO
 INNER JOIN Opioids_DW.dbo.drug_dim dr ON dr.drug_name = ar.drug_NAME
-WHERE --b.buyer_state = 'MA' AND 
-dosage_unit != '999' AND quantity != '999' AND CALC_BASE_WT_IN_GM != '0' AND REPORTER_DEA_NO = 'PM0020850'
+WHERE dosage_unit != '999' AND quantity != '999' AND CALC_BASE_WT_IN_GM != '0' AND REPORTER_DEA_NO = 'PM0020850'
 AND date_key = 1
 GROUP BY t.date_key, dr.drug_key, di.distributor_key, b.buyer_key
 
